f5fef7c83d55c8fef0416146fafbf88fb8c63ecd
Lemmar Wilson <lemmar.wilson@outlook.com>
2023-07-02 23:37:33 -0700

    session 4

--- project/project/urls.py
+++ project/project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', user_views.login, name='login')
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'), name='login')
 ]
 
 
